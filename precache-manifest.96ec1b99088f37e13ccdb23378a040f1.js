bb5b0a4b2a490958a5706b3e68eaccc8c4ca89c9
Brahm Harsh Parmar <40244239+brahmharsh@users.noreply.github.com>
2020-04-21 18:54:31 +0530

    Add files via upload

---
+++ precache-manifest.96ec1b99088f37e13ccdb23378a040f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a18cfc78c4200d2d66c8672b0ae2c02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef6dbd45eaeb8c53e1f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5d399aaac1abea45fe04",
+    "url": "/static/js/2.9859acc9.chunk.js"
+  },
+  {
+    "revision": "9b106367bfdd2f660fb9d31b8b1921c8",
+    "url": "/static/js/2.9859acc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef6dbd45eaeb8c53e1f",
+    "url": "/static/js/main.ba06684a.chunk.js"
+  },
+  {
+    "revision": "0360fb5ea80ed96baaff",
+    "url": "/static/js/runtime-main.48bd821b.js"
+  }
+]);
